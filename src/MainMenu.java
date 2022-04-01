87d0cfc3a3175839bdeea1f0779d70b5d0a9079a
elliot-lyons <elliot243lyons@gmail.com>
2022-04-01 18:52:05 +0100

    Still managing input. Something goes wrong when stops aren't consecutive IDs

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -6,32 +6,34 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MainMenu
-{
+public class MainMenu {
     private boolean quit, first, firstRoute, firstStop, firstArrive;
     private Scanner s;
     private ArrayList<Stop> stops;
     private ArrayList<Trip> trips;
     private double[][] distances;
-    private String[][] tripIDs;
+    private String[][] tripIDs, sources, dests;
+    RoutePlan routePlan;
     //private Distance[][] dists;
 
-    public MainMenu(Scanner s)
-    {
+    public MainMenu(Scanner s) {
         this.s = s;
         quit = false;
         first = true;
+
         stops = createStops();
-        //distances = createDistances(stops, tripIDs);
-        //dists = createDists();
-        //trips = tripList();
+        trips = new ArrayList<Trip>();
+        tripIDs = new String[stops.size()][stops.size()];
+        sources = new String[stops.size()][stops.size()];
+        dests = new String[stops.size()][stops.size()];
+        distances = createDistances(stops);
         firstRoute = true;
+
         firstStop = true;
         firstArrive = true;
     }
 
-    public boolean isQuit()
-    {
+    public boolean isQuit() {
         return quit;
     }
 
@@ -65,17 +67,14 @@ public class MainMenu
 //        return result;
 //    }
 
-    public ArrayList<Stop> createStops()
-    {
+    public ArrayList<Stop> createStops() {
         ArrayList<Stop> result = new ArrayList<Stop>();
 
-        try
-        {
-            BufferedReader br = new BufferedReader(new FileReader("transit_files//stops.txt"));
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("transit_files//smaller_stops.txt"));
             String current = br.readLine();
 
-            while ((current = br.readLine()) != null)
-            {
+            while ((current = br.readLine()) != null) {
                 String[] line = current.split(",", -1);
 
                 String id = line[0];
@@ -85,82 +84,65 @@ public class MainMenu
                 result.add(s);
             }
 
-        }
-
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("stops.txt file not found");
         }
 
         return result;
     }
 
-    /**
-     * Might have to be a String[][] twice as long holding tripIDs. (Maybe only twice as long lengthwise)
-     */
-
-    public double[][] createDistances(ArrayList<Stop> stops, String[][] tripIDs)
-    {
+    public double[][] createDistances(ArrayList<Stop> stops) {
         double[][] result = new double[stops.size()][stops.size()];
 
-        for (int i = 0; i < result.length; i++)
-        {
-            for (int j = 0; j < result.length; j++)
-            {
-                if (i == j)
-                {
+        for (int i = 0; i < stops.size(); i++) {
+            for (int j = 0; j < stops.size(); j++) {
+                if (i == j) {
                     result[i][j] = 0;
                     tripIDs[i][j] = "Same stop";
-                }
-
-                else
-                {
+                } else {
                     result[i][j] = Double.POSITIVE_INFINITY;
                     tripIDs[i][j] = "n/a";
                 }
             }
         }
 
-        try
-        {
-            BufferedReader br = new BufferedReader(new FileReader("transit_files//small_stop_times.txt"));
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("transit_files//smaller_stop_times.txt"));
             String current = br.readLine();
             String[] line = current.split(",", -1);
             String previous = line[0];
             previous = "n/a";
             boolean first = true;
 
-            while(current != null)
-            {
-                if (first)
-                {
+            while (current != null) {
+                if (first) {
                     current = br.readLine();
                     line = current.split(",", -1);
+                    first = false;
                 }
 
                 ArrayList<String> stopIDs = new ArrayList<String>();
                 ArrayList<Integer> sequences = new ArrayList<Integer>();
 
                 String tripID = line[0];
+
                 String stopID = line[3];
                 int sequence = Integer.parseInt(line[4]);
 
                 Trip trip = new Trip(tripID);
                 int maxIndex = 0;
 
-                do
-                {
-                    try
-                    {
-                        if (first)
-                        {
-                            first = false;
-                        }
-
-                        if (sequence > maxIndex)
-                        {
+                do {
+                    try {
+                        if (sequence > maxIndex) {
                             maxIndex = sequence;
                         }
+
+//                        if (tripID.equals("12960508"))
+//                        {
+//                            System.out.println("yes");
+//                            System.out.println(stopID);
+//                        }
 
                         stopIDs.add(stopID);
                         sequences.add(sequence);
@@ -168,130 +150,112 @@ public class MainMenu
                         previous = line[0];
                         current = br.readLine();
 
-                        if (current != null)
-                        {
+                        if (current != null) {
                             line = current.split(",", -1);
                             tripID = line[0];
                             stopID = line[3];
                             sequence = Integer.parseInt(line[4]);
-                        }
-
-                        else
-                        {
+                        } else {
                             break;
                         }
-                    }
-
-                    catch (NullPointerException e)
-                    {
+                    } catch (NullPointerException e) {
                         System.out.println("All ok!");
                     }
+
                 } while (tripID.equals(previous));
 
                 String[] tripStops = new String[maxIndex];
 
-                for (int i = 0; i < tripStops.length; i++)
+                if (tripID.equals("12960508"))
                 {
+                    System.out.println("Ok");
+                    for (int i = 0; i < stopIDs.size(); i++) {
+                        System.out.println(stopIDs.get(i));
+                    }
+                }
+
+                for (int i = 0; i < tripStops.length; i++) {
                     int index = sequences.get(i) - 1;
                     String id = stopIDs.get(i);
                     tripStops[index] = id;
                 }
 
                 trip.setStops(tripStops);
+                trips.add(trip);
+
 
                 for (int i = 0; i < tripStops.length; i++)                      // adding in the shortest direct paths
                 {                                                               // between nodes
-                    for (int j = i + 1; j < tripStops.length; j++)
-                    {
+                    for (int j = i + 1; j < tripStops.length; j++) {
                         double dis = (double) j - 1;
                         int indexI = findIndex(tripStops[i]);
-//                        System.out.println("i: " + tripStops[i] + " ind: " + indexI);
                         int indexJ = findIndex(tripStops[j]);
-//                        System.out.println("j: " + tripStops[j] + "ind: " + indexJ);
-//                        System.out.println("Distance:" + dis);
 
-                        if (indexI < 0 || indexJ < 0)
-                        {
+                        if (indexI < 0 || indexJ < 0) {
                             System.out.println("Error: indexI: " + indexI + " indexJ " + indexJ);
-                        }
-
-                        else
-                        {
-                            if (dis < result[indexI][indexJ])
-                            {
+                        } else {
+                            if (dis < result[indexI][indexJ]) {
                                 result[indexI][indexJ] = dis;
                                 tripIDs[indexI][indexJ] = tripID;
+                                sources[indexI][indexJ] = tripStops[i];
+                                dests[indexI][indexJ] = tripStops[j];
                             }
                         }
                     }
                 }
             }
-        }
-
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("stop_times.txt not found");
         }
 
+        /**
+         * Uncomment transfers
+         */
+
         // getting shortest costs from transfers.txt
-        try
-        {
-            BufferedReader br = new BufferedReader(new FileReader("transit_files//transfers.txt"));
-            String current = br.readLine();
-            int count = 1;                  // used for letting user know the line no. of error if there is one in the file
-
-            while ((current = br.readLine()) != null)
-            {
-                String[] line = current.split(",", -1);
-                count++;
-
-                String source = line[0];
-                String destination = line[1];
-                int type = Integer.parseInt(line[2]);
-                double dist = -1;
-
-                if (type == 2)
-                {
-                    dist = Double.parseDouble(line[3]) / 100;
-                }
-
-                else if (type == 0)
-                {
-                    dist = 2;
-                }
-
-                else
-                {
-                    System.out.println("Error, invalid transfer type in transfers.txt for journey from stop: " + source
-                    + " to stop " + destination + " on line " + count + ".");
-                }
-
-                int indexI = findIndex(source);
-                int indexJ = findIndex(destination);
-                System.out.println("Distance: " + dist);
-
-                if (dist < result[indexI][indexJ])
-                {
-                    result[indexI][indexJ] = dist;
-                    tripIDs[indexI][indexJ] = "Transfer";
-                }
-            }
-        }
-
-        catch (IOException e)
-        {
-            System.out.println("transfers.txt not found");
-        }
+//        try {
+//            BufferedReader br = new BufferedReader(new FileReader("transit_files//transfers.txt"));
+//            String current = br.readLine();
+//            int count = 1;                  // used for letting user know the line no. of error if there is one in the file
+//
+//            while ((current = br.readLine()) != null) {
+//                String[] line = current.split(",", -1);
+//                count++;
+//
+//                String source = line[0];
+//                String destination = line[1];
+//                int type = Integer.parseInt(line[2]);
+//                double dist = -1;
+//
+//                if (type == 2) {
+//                    dist = Double.parseDouble(line[3]) / 100;
+//                } else if (type == 0) {
+//                    dist = 2;
+//                } else {
+//                    System.out.println("Error, invalid transfer type in transfers.txt for journey from stop: " + source
+//                            + " to stop " + destination + " on line " + count + ".");
+//                }
+//
+//                int indexI = findIndex(source);
+//                int indexJ = findIndex(destination);
+//
+//                if (dist < result[indexI][indexJ]) {
+//                    result[indexI][indexJ] = dist;
+//                    tripIDs[indexI][indexJ] = "a transfer";
+//                    sources[indexI][indexJ] = source;
+//                    dests[indexI][indexJ] = destination;
+//                }
+//            }
+//        } catch (IOException e) {
+//            System.out.println("transfers.txt not found");
+//        }
 
         return result;
     }
 
-    public int findIndex(String stopID)
-    {
-        for (int i = 0; i < stops.size(); i++)
-        {
-            if (stops.get(i).getStopID().equals(stopID))
-            {
+    public int findIndex(String stopID) {
+        for (int i = 0; i < stops.size(); i++) {
+            if (stops.get(i).getStopID().equals(stopID)) {
                 return i;
             }
         }
@@ -299,9 +263,29 @@ public class MainMenu
         return -1;
     }
 
-    public String[][] getTripIDs()
-    {
+    public String[][] getTripIDs() {
         return tripIDs;
+    }
+
+    public void floydWarshall()
+    {
+        System.out.println(distances.length);
+        for (int i = 0; i < distances.length; i++)
+        {
+            for (int j = 0; j < distances.length; j++)
+            {
+                for (int k = 0; k < distances.length; k++)
+                {
+                    if (distances[j][i] + distances[i][k] < distances[j][k])
+                    {
+                        distances[j][k] = distances[j][i] + distances[i][k];
+                        tripIDs[j][k] = tripIDs[j][i] + "," + tripIDs[i][k];
+                        sources[j][k] = sources[j][i] + "," + sources[i][k];
+                        dests[j][k] = dests[j][i] + "," + dests[i][k];
+                    }
+                }
+            }
+        }
     }
 
     //    public Distance[][] createDists()
@@ -448,12 +432,9 @@ public class MainMenu
 //        return result;
 //    }
 
-    public void display()
-    {
-        while (!quit)
-        {
-            if (first)
-            {
+    public void display() {
+        while (!quit) {
+            if (first) {
                 System.out.println("Please choose from one of the following options by pressing the corresponding key:");
                 System.out.println("1: Plan a route");
                 System.out.println("2: See stop info");
@@ -462,85 +443,45 @@ public class MainMenu
                 first = false;
             }
 
-            try
-            {
+            try {
                 String input = s.nextLine();
 
-                switch (input)
-                {
-                    case "1":
-                    {
-                        if (firstRoute)
-                        {
+                switch (input) {
+                    case "1": {
+                        if (firstRoute) {
                             firstRoute = false;
-
-                            Trip t = trips.get(1);
-
-                            System.out.println(t.getTripID());
-                            stops = createStops();
-                            tripIDs = new String[stops.size()][stops.size()];
-                            distances = createDistances(stops, tripIDs);
-
-                            System.out.println(distances[2][2]);
-
-                            for (int i = 0; i < distances.length; i++)
-                            {
-                                for (int j = 0; j < distances[0].length; j++)
-                                {
-                                    if (!tripIDs[i][j].equals("n/a") && !tripIDs[i][j].equals("Transfer") && !tripIDs[i][j].equals("Same stop"))
-                                    {
-                                        System.out.println("Distance: " + distances[i][j] + "Trip ID " + tripIDs[i][j]);
-                                    }
-                                }
-                            }
+                            floydWarshall();
+                            routePlan = new RoutePlan(s, distances, tripIDs, stops, trips, sources, dests);
                         }
-                        //RoutePlan routePlan = new RoutePlan(s, stops);
-                        //routePlan.display();
+
+                        routePlan.display();
                         break;
                     }
 
-                    case "2":
-                    {
+                    case "2": {
                         StopInfo stopInfo = new StopInfo(s);
                         stopInfo.display();
                         break;
                     }
 
-                    case "3":
-                    {
-                        ArrivalTime arrivalTime = new ArrivalTime(s,trips);
+                    case "3": {
+                        ArrivalTime arrivalTime = new ArrivalTime(s, trips);
                         arrivalTime.display();
                         break;
                     }
 
-                    case "4":
-                    {
+                    case "4": {
                         quit = true;
                         break;
                     }
 
-                    default:
-                    {
+                    default: {
                         System.out.println("Invalid input");
                         first = true;
                     }
                 }
+            } catch (Exception e) {
             }
-
-            catch (Exception e)
-            {}
         }
-    }
-
-    public int findDistance(String[] arrive, String[] depart)
-    {
-        int hours = Integer.parseInt(arrive[0]) - Integer.parseInt(depart[0]);
-        int mins = Integer.parseInt(arrive[1]) - Integer.parseInt(depart[1]);
-        int secs = Integer.parseInt(arrive[2]) - Integer.parseInt(depart[2]);
-
-        hours = hours * 60 * 60;                        // converting into seconds
-        mins *= mins;
-
-        return hours + mins + secs;
     }
 }
